610156878a7091e51b9fbb34bb281e4f6d598465
Kate Popova <popova1919@yandex.ru>
2025-08-04 21:15:22 +0530

    Test init

---
+++ src/test/java/ru/netology/web/FillingFormTest.java
@@ -0,0 +1,55 @@
+package ru.netology.web;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class FillingFormTest {
+    private WebDriver driver;
+
+    @BeforeAll
+    static void setUpAll() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    void setUp() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+        driver.get("http://localhost:9999");
+    }
+
+    @AfterEach
+    void tearDown() {
+        driver.quit();
+        driver = null;
+    }
+
+    @Test
+    void shouldTestV1() {
+        List<WebElement> elements = driver.findElements(By.className("input__control"));
+//        driver.findElements(By.cssSelector("input[type='text']")).sendKeys("Екатерина");
+//        driver.findElements(By.cssSelector("input[type='tel']")).sendKeys("+71234567890");
+        elements.get(0).sendKeys("Екатерина");
+        elements.get(1).sendKeys("+71234567890");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.className("button")).click();
+        String expected = "  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        String actual = driver.findElement(By.className("Success_successBlock__2L3Cw")).getText();
+        assertEquals(expected, actual);
+    }
+}
